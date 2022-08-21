3a9a89023e6810cb62f447a6d1bcc16ea4e44071
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2022-08-21 19:48:43 +0430

    part two header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,17 +2,17 @@ import React from "react";
 import "./Header.css";
 import PersonIcon from "@mui/icons-material/Person";
 import IconButton from "@material-ui/core/IconButton";
+import ForumIcon from "@mui/icons-material/Forum";
 function Header() {
   return (
     <div className="header">
       <IconButton>
         <PersonIcon fontSize="large" className="header__icon" />
       </IconButton>
-      <img
-        className="header__logo"
-        src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"
-        alt="logo"
-      />
+      <img className="header__logo" src="../../../public/77.png" />
+      <IconButton>
+        <ForumIcon fontSize="large" className="header__icon" />
+      </IconButton>
     </div>
   );
 }
